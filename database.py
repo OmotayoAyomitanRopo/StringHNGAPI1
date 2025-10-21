fb940f798aba4f58eb6152ef16054c240d2c25de
unknown <OmotayoAyomitanRopo>
2025-10-21 08:36:58 -0700

    In-memeory storage implemented

---
+++ database.py
@@ -0,0 +1,40 @@
+from typing import Dict
+from logic import analyze_string
+from datetime import datetime
+import hashlib
+
+# An empty DB that will serve as in-memeory database
+DB: Dict[str, dict] = {}
+
+# Function definition to store analyzed property
+def create_string(value: str):
+    # calling the string analyzer function to retrieve the
+    # Property of the input string
+    propert = analyze_string(value)
+
+    # Extracting the SHA-256 hash, whiich will be used as a unique property
+    hash_id = propert["SHA256_hash"]
+
+    if hash_id in DB: #If the unque identifier (hash_id) already exist in database
+        return None
+    DB[hash_id] = {
+        "id": hash_id,
+        "value": value,
+        "properties": propert,
+        "created_at": datetime.utcnow().isoformat() + "Z"
+    }
+    return DB[hash_id]
+
+#It will retrieve it properties from database
+def get_string_by_value(value: str):
+    hash_id = hashlib.sha256(value.encode()).hexdigest()
+    return DB.get(hash_id)
+
+# It will delete a string from the database
+def delete_string(value: str):
+    hash_id = hashlib.sha256(value.encode()).hexdigest()
+    return DB.pop(hash_id, None)
+
+#This will return all the dictionary value in the in-memory.
+def get_all_strings():
+    return list(DB.values())
