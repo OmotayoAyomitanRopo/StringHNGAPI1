f2dcac392aca0cb11e1fe34500507f149b84b63c
unknown <OmotayoAyomitanRopo>
2025-10-21 08:34:50 -0700

    Filtering helper function here

---
+++ utils.py
@@ -0,0 +1,20 @@
+def use_filters(data, filters):
+    results = data
+
+    if filters.get("is_palindrome") is not None:
+        results = [x for x in results if x["properties"]["is_palindrome"] == filters["is palindrom"]]
+
+    if filters.get("min_length") is not None:
+        results = [x for x in results if x["properties"]["length"] >= filters["min_length"]]
+
+    if filters.get("max_length") is not None:
+        results = [x for x in results if x["properties"]["length"] <= filters["max_length"]]
+
+    if filters.get("word_count") is not None:
+        results = [x for x in results if x["properties"]["word_count"] == filters["word_count"]]
+
+    if filters.get("contains_char") is not None:
+        char = filters["contains_char"]
+        results = [x for x in results if char in x["value"]]
+
+    return results
